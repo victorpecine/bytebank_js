2cc76ddf0eca1c69884a8373787bf814d2a5b670
Victor Pecine <victorpecine@gmail.com>
2021-03-22 19:22:40 -0300

    Saldo como atributo privado

--- index.js
+++ index.js
@@ -5,15 +5,23 @@ class Cliente {
 
 class ContaCorrente {
     agencia;
-    saldo;
+    _saldo = 0; // atributo privado
 
     sacar(valor) {
-        if (this.saldo >= valor) {
-            this.saldo -= valor;
-            console.log(`\nRealizado saque de R$ ${valor}`);
-            console.log(`\n>>> Saldo atual: R$ ${this.saldo} <<<`);
+        if (this._saldo >= valor) {
+            this._saldo -= valor;
+            console.log(`\nRealizado o saque de R$ ${valor}`);
+            console.log(`\n>>> Saldo atual: R$ ${this._saldo} <<<`);
         } else {
             console.log(`\nNão há saldo suficiente para sacar R$ ${valor}`);
+        }
+    }
+
+    depositar(valor) {
+        if (valor > 0) {
+            this._saldo += valor;
+            console.log(`\nDepositado o valor de R$ ${valor}`);
+            console.log(`\n>>> Saldo atual: R$ ${this._saldo}`);
         }
     }
 }
@@ -24,6 +32,5 @@ cliente1.cpf = 08743567321;
 
 const contaCorrenteVictor = new ContaCorrente();
 contaCorrenteVictor.agencia = 1001;
-contaCorrenteVictor.saldo = 100;
-
+contaCorrenteVictor.depositar(250);
 contaCorrenteVictor.sacar(200);
